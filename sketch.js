1c38baa1065ebc936e43c43d1ac633d99be4f6ef
F4Tornado <h.rovnyak@gmail.com>
2018-03-23 16:47:43 -0400

    thingoos

--- sketch.js
+++ sketch.js
@@ -1,15 +1,34 @@
 function setup() {
   createCanvas(windowWidth, windowHeight).position(0, 0).parent("#container");
+  load([{
+    type: "image",
+    name: "start",
+    path: "assets/start.png"
+  }])
 }
 
 function draw() {
-  image(assets.start, 0, 0, width, height);
+  if (gameData.page === 0) {
+
+  } else if (gameData.page === 1) {
+    image(assets.start, 0, 0, width, height);
+    fill("#c46200");
+    rectMode(CENTER);
+    noStroke();
+    rect(width / 2, height / 2, width / 8, height / 12);
+    fill("#c400c4");
+    textSize(width / 54);
+    text("Join game", width / 2 - textWidth("Join game") / 2, height / 2 + width / 108);
+    fill("#c46200");
+    rectMode(CENTER);
+    noStroke();
+    rect(width / 2, height / 2 - height / 8, width / 8, height / 12);
+    fill("#c400c4");
+    textSize(width / 54);
+    text("Create game", width / 2 - textWidth("Create game") / 2, height / 2 - height / 8 + width / 108);
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function preload() {
-  assets.start = loadImage("assets/start.png");
 }
--- vars.js
+++ vars.js
@@ -1,1 +1,17 @@
-assets = {}
+assets = {};
+
+gameData = {
+  page:0
+};
+
+const load = (things, index) => {
+  if (things[index].type = "image") {
+    assets[things[index].name] = loadImage(things[index].path, () => {
+      if (index < things.length) {
+        load(things, index++);
+      } else {
+        gameData.page = 1;
+      }
+    })
+  }
+}
